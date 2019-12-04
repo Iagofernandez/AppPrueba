1a778aebde88e59a1c1736d7b27aedf8e1a1137f
Iago <ifernandezblanco@danielcastelao.org>
2019-12-04 09:27:22 +0100

    AppPrueba de permisos de Camara en android studio

--- app/src/main/java/com/example/apppermisos/MainActivity.kt
+++ app/src/main/java/com/example/apppermisos/MainActivity.kt
@@ -1,17 +1,72 @@
 package com.example.apppermisos
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
+
+const val MY_PERMISSIONS_REQUEST_CAMERA = 1;
+const val REQUEST_IMAGE_CAPTURE = 2;
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        toast("Se inicia la app")
+
+        btnCheck.setOnClickListener{checkPermisoCamara()}
+        btnFoto.setOnClickListener{dispachTakePictureIntent()}
 
     }
+
+    fun checkPermisoCamara() {
+
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            toast("Sin permisos garantizados")
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.CAMERA), MY_PERMISSIONS_REQUEST_CAMERA
+            )
+        } else {
+            toast("Permiso Activado")
+
+        }
+    }
+            private fun dispachTakePictureIntent(){
+                Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                    takePictureIntent.resolveActivity(packageManager)?.also {
+                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data!!.extras!!.get("data") as Bitmap
+            imagen.setImageBitmap(imageBitmap)
+        }
+    }
+
+
+
+
 
 
 }
